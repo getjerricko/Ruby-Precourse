d5e3ccece817f0c424815855949de2ae1ba2c4cc
getjerricko <getjerricko@gmail.com>
2014-08-30 16:04:53 -0400

    added variables

--- 01_the_basics/exercise_1.rb
+++ 01_the_basics/exercise_1.rb
@@ -1,2 +1,8 @@
-
+# Simple concatenation
 puts "Jerrick " + "Balatbat"
+
+# Concatenation using variable and string interpolation
+first_name = "Jerrick"
+last_name = "Balatbat"
+
+puts "#{first_name} #{last_name}"
