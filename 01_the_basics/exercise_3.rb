367442b8054551af25b50514e86d9b0b1c0560e1
getjerricko <getjerricko@gmail.com>
2014-08-30 17:01:21 -0400

    init commit

---
+++ 01_the_basics/exercise_3.rb
@@ -0,0 +1,12 @@
+# Movie titles with the year they came out
+movies = { :"The Matrix" => 1998, 
+           :"Jaws"       => 1980, 
+           :"Gravity"    => 2013, 
+           :"Red Planet" => 2006 }
+
+# Print out the year of each movie 
+puts movies[:"The Matrix"]
+puts movies[:"Jaws"]
+puts movies[:"Gravity"]
+puts movies[:"Red Planet"]
+
