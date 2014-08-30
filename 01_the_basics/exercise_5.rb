1199c445c2c40353f526b74505c8ec4f032c08a0
getjerricko <getjerricko@gmail.com>
2014-08-30 17:35:02 -0400

    init commit

---
+++ 01_the_basics/exercise_5.rb
@@ -0,0 +1,11 @@
+# Output factorials of numbers 5, 6, 7, and 8
+
+FIVE =  5 * 4 * 3 * 2 * 1
+SIX =  6 * 5 * 4 * 3 * 2 * 1
+SEVEN =  7 * 6 * 5 * 4 * 3 * 2 * 1
+EIGHT =  8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
+
+puts "#{FIVE} is the factorial of 5."
+puts "#{SIX} is the factorial of 6."
+puts "#{SEVEN} is the factorial of 7."
+puts "#{EIGHT} is the factorial of 8."
