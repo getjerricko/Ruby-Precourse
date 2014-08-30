b3da409c78d71737223940a4a78ece97f4fdf0af
getjerricko <getjerricko@gmail.com>
2014-08-30 19:47:37 -0400

    init commit

---
+++ 02_variables/04_name.rb
@@ -0,0 +1,12 @@
+# 04_name.rb
+# Prompts then saves the user his/her first and last names
+
+puts "Hi. What's your first name? "
+print ">> "
+first_name = gets.chomp
+
+puts "And your last name? "
+print ">> "
+last_name = gets.chomp
+
+puts "Well, hello there #{first_name} #{last_name}. Nice to meet you. "
