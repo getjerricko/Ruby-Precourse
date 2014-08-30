4266456340de6ceda51ad8df7e46b5dc47822a9b
getjerricko <getjerricko@gmail.com>
2014-08-30 19:19:52 -0400

    init commit

---
+++ 02_variables/01_name.rb
@@ -0,0 +1,9 @@
+# 01_name.rb
+# Prompts the user his/her name and prints out 
+# a greeting message with the name included
+
+puts "Hi. What's your name, stranger? "
+print ">> "
+name = gets.chomp
+
+puts "Well, hello there #{name}. Nice to meet you. "
