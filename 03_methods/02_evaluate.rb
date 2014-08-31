a28928395f08206744a7d65a1ed12e9f7b31db1a
getjerricko <getjerricko@gmail.com>
2014-08-30 22:15:25 -0400

    init commit

---
+++ 03_methods/02_evaluate.rb
@@ -0,0 +1,12 @@
+# 02_evaluate.rb
+# Evaluate the following expressions
+
+x = 2              # 2
+
+puts x = 2         # nil
+
+p name = "Joe"     # "Joe"
+
+four = "four"      # "four"
+
+print something = "nothing"  # nil
