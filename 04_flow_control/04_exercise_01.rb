6c4158391d0aa16685ea8b4f264daaa971bc9071
getjerricko <getjerricko@gmail.com>
2014-08-31 15:16:26 -0400

    init commit

---
+++ 04_flow_control/04_exercise_01.rb
@@ -0,0 +1,17 @@
+# 04_exercise_01.rb
+# Write down whether the following expressions
+# return true or false.
+
+(32 * 4) >= 129          # false
+
+false != !true           # false
+
+true == 4                # false 
+
+false == (874 == '874')  # true
+
+(!true ||(!(100 / 5) == 20) || ((328 / 4) == 82)) || false  # true
+
+
+
+
