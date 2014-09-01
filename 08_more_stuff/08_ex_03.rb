e02c0aa08ef3ed4fe93114bb64667dc018466e9f
getjerricko <getjerricko@gmail.com>
2014-09-01 16:58:55 -0400

    init commit

---
+++ 08_more_stuff/08_ex_03.rb
@@ -0,0 +1,8 @@
+# 08_ex_03.rb
+# What is exception handling and what problem does it solve?
+#
+# Exception handing is for handling different errors types
+# which may happen during the course of executing a program.
+# It takes the appropriate action instead of stopping program 
+# completely.
+
