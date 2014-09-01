8f647c06ebed8f5855a9846b7a5950ed7ef712b4
getjerricko <getjerricko@gmail.com>
2014-09-01 13:45:24 -0400

    init commit

---
+++ 07_hashes/07_ex_07.rb
@@ -0,0 +1,10 @@
+# 07_ex_07.rb
+# what's the difference between 
+# the two hashes
+
+x = "hi there"
+my_hash = {x: "some value"}
+my_hash2 = {x => "some value"}
+
+# my_hash has a symbol key.
+# my_hash2 has a string key from x's value.
