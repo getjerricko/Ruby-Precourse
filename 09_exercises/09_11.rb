59753e86a6b86391934d8ab914062ded7dd5574b
getjerricko <getjerricko@gmail.com>
2014-09-01 19:38:02 -0400

    init commit

---
+++ 09_exercises/09_11.rb
@@ -0,0 +1,9 @@
+# 09_11.rb
+# Look at several Rails/Ruby online API sources 
+# and say which one you like best and why.
+#
+# I mainly use http://ruby-doc.org/. The layout is 
+# intuitive and organized. Another one I use is 
+# 'ri' which is included in the ruby installation. 
+# It uses the terminal so it's  quick and available 
+# offline.
