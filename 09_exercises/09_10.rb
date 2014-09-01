3badc2165dd661bb1f9e7b319527c9893a58b40a
getjerricko <getjerricko@gmail.com>
2014-09-01 19:30:41 -0400

    init commit

---
+++ 09_exercises/09_10.rb
@@ -0,0 +1,11 @@
+# 09_10.rb
+# Can hash values be arrays?
+# Can you have an array of hashes?
+
+# Yes.
+
+# hash with array values
+hash = {ninja_turles: ['leo', 'don', 'raph', 'mikey']}
+#
+# # array of hashes
+array = [{name: 'leo'}, {name: 'don'}, {name: 'raph'}, {name: 'mikey'}]
